c3ebdfb47150bbce2a03af9743b41d74f416f9b0
Noor ul Huda <noor.huda@rainsoftfn.com>
2021-11-23 00:04:18 +0500

    user register route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,21 +3,29 @@ const User = require('../models/user')
 
 const router = express.Router();
 
-router.post("/register", (req, res) => {
-    User.find({email: req.body.email}, (err, user) => {
-        if (user) {
-            return res.status(400).json({ message: "User already exist" })
-        }
-    })
+router.post("/register", async (req, res) => {
+    try {
+        User.find({ email: req.body.email }, (err, user) => {
+            if (user.length) {
+                res.status(400).json({ message: "User already exist" })
+            }
+            else {
+                let newUser = new User(req.body);
+                newUser.save(err => {
+                    if (!err)
+                        res.send("User registered Successfully")
+                    else
+                        res.send("Something went wrong")
 
-    let newUser = new User(req.body);
-    newUser.save(err => {
-        if(!err)
-            return res.send("User registered Successfully")
-        else
-            return res.status(400).json({ message: "Something went wrong" })
-            
-    })
+                })
+            }
+        })
+
+
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" })
+    }
+
 })
 
 module.exports = router;
